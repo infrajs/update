85e5f287954fdb6f979595fef957b2c7d514bd48
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-03-09 13:47:53 +0400

    C:/Program Files/Git/-update/ без удаления кэша

--- index.php
+++ index.php
@@ -10,10 +10,8 @@
 	if (Update::$is) return Ans::ret($ans, 'update итак запущен');
 
 	Access::test(true);	
-	Path::fullrmdir(Path::$conf['cache']);
+	//Path::fullrmdir(Path::$conf['cache']);
 	Update::exec();
-	
-
 	
 	return Ans::ret($ans);
 ?>
